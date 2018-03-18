dc8dbfa79de6248ceeca9d242ac9dff965592eb2
H3lli <H3lli@users.noreply.github.com>
2018-03-18 17:59:38 +0100

    Update fizzbuzz.py
    fix exception error

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,9 @@
 print ("Welcome to the FizzBuzz Game")
 number_int=raw_input("Please enter a number between 1 and 100: ")
-number_int=int(number_int)
+try:
+    number_int=int(number_int)
+except Exception as e: 
+    print("Please enter a whole number.")
 
 for number_int in range(1, number_int+1):
     if number_int % 3 == 0 and number_int % 5 == 0:
@@ -10,6 +13,4 @@ for number_int in range(1, number_int+1):
     elif  number_int % 5 == 0:
         print("buzz")
     else:
-        print number_int
-except Exception as e: 
-    print "Please enter a whole number."
+        print(number_int)
